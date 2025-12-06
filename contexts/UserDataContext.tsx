d2deac9decec77a8ff9549ac14814d759a6c960a
justinal64 <justinal8183@gmail.com>
2025-12-05 23:47:09 -0600

    removed import taht wasn't used

--- contexts/UserDataContext.tsx
+++ contexts/UserDataContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import React, { createContext, useContext, useState, ReactNode } from 'react';
 import { UserBookmark, UserNote } from '../types/bible';
 import { useAuth } from './AuthContext';
 
