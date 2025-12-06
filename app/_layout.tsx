c3d8f810693f93ee7ba045f74e4a0831146ca707
justinal64 <justinal8183@gmail.com>
2025-12-06 00:10:19 -0600

    removed context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ import { useFrameworkReady } from '../hooks/useFrameworkReady';
 import { ThemeProvider } from '../contexts/ThemeContext';
 import { AuthProvider } from '../contexts/AuthContext';
 import { ReaderProvider } from '../contexts/ReaderContext';
-import { UserDataProvider } from '../contexts/UserDataContext';
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -28,13 +28,11 @@ export default function RootLayout() {
     <ThemeProvider>
       <ReaderProvider>
         <AuthProvider>
-          <UserDataProvider>
             <Stack screenOptions={{ headerShown: false }}>
               <Stack.Screen name="(tabs)" />
               <Stack.Screen name="+not-found" />
             </Stack>
             <StatusBar style="auto" />
-          </UserDataProvider>
         </AuthProvider>
     </ReaderProvider>
     </ThemeProvider>
